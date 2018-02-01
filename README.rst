150284ff725576ed57143f20222d0636b208c840
Johan Hidding <johannes.hidding@gmail.com>
2018-02-01 17:04:48 +0100

    initial commit

---
+++ README.rst
@@ -0,0 +1,5 @@
+HyperCanny Climate module
+=========================
+
+Application of the (high dimensional) Canny edge detection algorithm to climate
+data.
---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+"""
+Setup script for the HyperCanny Climate module.
+"""
+
+from pathlib import Path
+from setuptools import setup
+
+# Get the long description from the README file
+here = Path(__file__).parent.absolute()
+with (here / 'README.rst').open(encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='HyperCanny Climate module',
+    version='0.1.0',
+    description='Runs the Canny edge detector on climate data.',
+    long_description=long_description,
+    author='Johan Hidding',
+    url='https://github.com/abrupt-climate/hypercc',
+    packages=['hypercc'],
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Intended Audience :: Science/Research',
+        'Environment :: Console',
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3.6'],
+    install_requires=[
+        'hyper_canny', 'noodles[numpy]', 'matplotlib', 'scipy', 'numpy'],
+    extras_require={
+        'develop': [
+            'pytest', 'pytest', 'coverage', 'pep8', 'tox',
+            'sphinx', 'sphinx_rtd_theme', 'nbsphinx'],
+    },
+)
