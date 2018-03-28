2b7d68615dadfb5da3464f4e2ca9729d6979945b
bathiany <sebastian.bathiany@wur.nl>
2018-03-28 07:47:47 +0000

    make python3.6 notation work in python3.5
    line 40/41, added: .format(variable=variable, ....)

--- hypercc/data/data_set.py
+++ hypercc/data/data_set.py
@@ -38,7 +38,7 @@ class DataSet(object):
     def cmip5(path, model: str, variable: str, scenario: str,
               realization: str, extension="nc", selection=slice(None)):
         pattern = f"{variable}_*mon_{model}_{scenario}" \
-                  f"_{realization}_??????-??????.{extension}"
+                  f"_{realization}_??????-??????.{extension}".format(variable=variable, model=model, scenario=scenario, realization=realization, extension=extension)
         paths = list(Path(path).glob(pattern))
         return DataSet(paths=paths, variable=variable, selection=selection)
 
